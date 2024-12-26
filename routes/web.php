22344262fb636476ef7fce6d02bebfed8cac340b
Bengie Villesco <bvillesco@gmail.com>
2024-12-27 00:02:55 +0800

    added several routes

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('login');
 });
+
+Route::get('/home', function () {
+    return view('home');
+})->name("homepage");
+
+Route::get('/properties', function () {
+    return view('Properties');
+})->name("propertiespage");
+
+Route::get('/findroommateortenant', function () {
+    return view('findRoommateorTenant');
+})->name("findroommateortenantpage");
+
+Route::get('/postaproperty', function () {
+    return view('postAProperty');
+})->name("postapropertypage");
+
+Route::get('/aboutus', function () {
+    return view('aboutUs');
+})->name("aboutuspage");
