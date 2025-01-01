a94f711efb08589243a4ec6618084b8b2b5ef615
Bengie Villesco <bvillesco@gmail.com>
2025-01-01 21:11:26 +0800

    add several route for redirecting to different pages.

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Serve login.html for the login page
 Route::get('/', function () {
-    return response()->file(public_path('html/login.html'));
+    return view('login');
 });
+
+Route::get('/home', function () {
+    return view('home');
+})->name("homepage");
+
+Route::get('/properties', function () {
+    return view('Properties');
+})->name("propertiespage");
+
+Route::get('/find-roommate-or-tenant', function () {
+    return view('find-Roommate-or-Tenant');
+})->name("findroommateortenantpage");
+
+Route::get('/post-a-property', function () {
+    return view('post-A-Property');
+})->name("postapropertypage");
+
+Route::get('/review', function () {
+    return view('review');
+})->name("reviewpage");
+
+Route::get('/userprofile', function () {
+    return view('user-profile');
+})->name("userprofilepage");
