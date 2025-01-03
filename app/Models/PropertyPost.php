85c86101880a6621a7dee413abcb4e368baad46b
Bengie Villesco <bvillesco@gmail.com>
2025-01-03 23:35:32 +0800

    - added code for casting the 'date_posted' to carbon instance for formatting. - added function to get the user's property post. - added function for average rating.

--- app/Models/PropertyPost.php
+++ app/Models/PropertyPost.php
@@ -4,11 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class PropertyPost extends Model
 {
     use HasFactory;
 
+    protected $casts = [
+        'date_posted' => 'datetime', // Ensure it's cast to a Carbon instance
+    ];
+
+    // Relationship of other tables in database
     public function user()
     {
         return $this->belongsTo(User::class);
@@ -22,5 +28,16 @@ class PropertyPost extends Model
     public function reviews()
     {
         return $this->hasMany(Reviews::class);
+    }
+
+    // Retrieve all data from property post
+    public static function getUserPropertyPost(){
+        // Get the property posts with related PropertyInfo and Reviews based on the id of the authenticated user
+        return self::where('user_id', Auth::id())->with(['propertyInfo', 'reviews'])->get();
+    }
+
+    // Average the ratings from reviews
+    public function averageRating(){
+        return $this->reviews->avg('rating');
     }
 }
