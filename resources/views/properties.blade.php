883baac9b81dcd85cf0d307ba087b02ef2144dc3
Bengie Villesco <bvillesco@gmail.com>
2025-01-04 18:22:21 +0800

    add 'Create a post' button with popover for property posting form

--- resources/views/properties.blade.php
+++ resources/views/properties.blade.php
@@ -1,11 +1,67 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Properties</title>
+
+    {{-- Link css and javascript file --}}
+    @vite('resources/css/properties.css')
 </head>
 <body>
     <x-navbar></x-navbar>
-    <h1>Properties Page</h1>
+    <div class="properties-container">
+        <h1>Properties Page</h1>
+
+        <div popover id="create-post-popover">
+            <h5>Create a Property Post</h5>
+
+            <!-- Form -->
+            <form action="" method="post">
+                <div class="form-container">
+                    <!-- Unit Category Dropdown Box -->
+                    <select name="unit-category" id="unit-category" required>
+                        <option value="" disabled selected>Please select a unit category</option>
+                        <option value="Dormitories">Dormitories</option>
+                        <option value="Studio Units">Studio Units</option>
+                        <option value="Apartments">Apartments</option>
+                        <option value="Boarding Houses">Boarding Houses</option>
+                        <option value="Other">Other</option>
+                    </select>
+
+                    <!-- City Dropdown Box -->
+                    <select name="city" id="city" required>
+                        <!-- The value of option should be dynamic -->
+                        <option value="" disabled selected>Please select city</option>
+                        <option value="Quezon City">Quezon City</option>
+                        <option value="Manila">Manila</option>
+                    </select>
+
+                    <!-- Barangay Dropdown Box -->
+                    <select name="barangay" id="barangay" required>
+                        <!-- The value of option should be dynamic.
+                            This dropdown should be disable until the city has a value.
+                        -->
+                        <option value="" disabled selected>Please select barangay</option>
+                        <option value="Holy Spirit">Holy Spirit</option>
+                        <option value="Tondo">Tondo</option>
+                    </select>
+
+                    <!-- input -->
+                    <input type="number" placeholder="Rental Price" id="rental-price" name="rental-price" required>
+                    <input type="number" placeholder="Maximum of Occupancy" id="max-occupancy" name="max-occupancy" required>
+                    <input type="text" placeholder="Write a description" id="description" name="description" required>
+
+                    <button>Add all photos</button>
+                    <button class="post-btn">Post</button>
+                </div>
+            </form>
+        </div>
+
+        <button popovertarget="create-post-popover">
+            Create a post
+        </button>
+    </div>
 </body>
 </html>
