23a613931a5c87ec4df1bf6d73774148a7cfa761
Bengie Villesco <bvillesco@gmail.com>
2025-01-13 16:12:25 +0800

    added getUserInfoAndItsFindingPost() method to query the database based on the needed data.

--- app/Models/FindRoommateOrTenant.php
+++ app/Models/FindRoommateOrTenant.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class FindRoommateOrTenant extends Model
 {
@@ -12,5 +13,20 @@ class FindRoommateOrTenant extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public static function getUserInfoAndItsFindingPost(){
+        $posts = DB::table('find_roommate_or_tenants as find_post')
+                    ->join('users', 'find_post.user_id', '=', 'users.id')
+                    ->select('find_post.*', 'users.firstname', 'users.lastname')
+                    ->orderByDesc('find_post.date_posted')
+                    ->get();
+
+        // Convert date_posted to Carbon instance
+        $posts->each(function ($post) {
+            $post->date_posted = \Carbon\Carbon::parse($post->date_posted);
+        });
+
+        return $posts;
     }
 }
