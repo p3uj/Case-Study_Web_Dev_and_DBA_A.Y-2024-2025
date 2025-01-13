9bd675a6dcb76a14054196723f537da2d20d06dc
Bengie Villesco <bvillesco@gmail.com>
2025-01-13 17:50:17 +0800

    added getUserAuthInfo() method

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
 
 class User extends Authenticatable
 {
@@ -68,5 +69,9 @@ class User extends Authenticatable
     public function reviews()
     {
         return $this->hasMany(Reviews::class);
+    }
+
+    public static function getUserAuthInfo() {
+        return Auth::user();
     }
 }
