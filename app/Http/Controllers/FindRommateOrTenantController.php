81d2b1f62ac25f044e96ab82788e30fce6a08d3b
Bengie Villesco <bvillesco@gmail.com>
2025-01-13 15:49:16 +0800

    Refactor FindRommateOrTenantController: Extract methods for user authentication and city list fetching
    - Moved user authentication logic to a private method 'userAuthInfo()'.
    - Extracted city list fetching logic into a private method 'showCityList()'.
    - Updated the 'index()' method to utilize the extracted methods and pass necessary data to the view.
    - Used 'FindRoommateOrTenant::getUserInfoAndItsFindingPost()' to fetch user posts related to their listings.

--- app/Http/Controllers/FindRommateOrTenantController.php
+++ app/Http/Controllers/FindRommateOrTenantController.php
@@ -3,26 +3,38 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Api\CityController;
+use App\Models\FindRoommateOrTenant;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 
 class FindRommateOrTenantController extends Controller
 {
-    public function userInfo(){
-        return view('user-profile', [
-            'user' => Auth::user()
-        ]);
+    private function userAuthInfo(){
+        return Auth::user();
     }
 
-    public function showCityList(){
+    private function showCityList(){
         // Call the CityController to fetch the data from the external API
         $cities = CityController::index();
-        //$barangays = BarangayController::index();
+
+        return $cities;
+    }
+
+    public function index(){
+        // Call the userAuthInfo() method to retrieve the authenticated user's information
+        $user = $this->userAuthInfo();
+
+        // Call the showCityList() method to fetch the city
+        $cityList = $this->showCityList();
+
+        // Use the User model to call the getUserInfoAndItsFindingPost() method to get user info and their related posts
+        $usersPosts = FindRoommateOrTenant::getUserInfoAndItsFindingPost();
 
         return view('find-roommate-or-tenant', [
-            'user' => Auth::user(),
-            'cities' => $cities,
-            //'barangays' => $barangays
+            'user' => $user
+            ,'cities' => $cityList
+            ,'posts' => $usersPosts
         ]);
     }
 }
