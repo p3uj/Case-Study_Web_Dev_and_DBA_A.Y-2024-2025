031d2a54e939fa49dc14acfbc400cec29b1e6379
Bengie Villesco <bvillesco@gmail.com>
2025-01-13 18:19:33 +0800

    Refactor: - remove the userInfo() method. - make the propertyPost() method as private and return the value to  the calling method. - added findRoommateOrTenantPost() with returning a value to the calling method. - added index() method to call different method and model to fetch the authenticated user info, its property post, and its find roommate/tenant post.

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -2,27 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\FindRoommateOrTenant;
 use App\Models\PropertyInfo;
 use App\Models\PropertyPost;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class UserProfileController extends Controller
 {
-    public function userInfo(){
-        return view('user-profile', [
-            'user' => Auth::user()
-        ]);
-    }
-
-    function propertyPost(){
+    private function propertyPost() {
         // Call the PropertyPost model to get the property posts of the authenticated user
         $propertyPost = PropertyPost::getUserPropertyPost();
 
+        return $propertyPost;
+    }
+
+    private function findRoommateOrTenantPost() {
+        // Call the FindRoommateOrTenant model to fetch posts for the authenticated user
+        $findPost = FindRoommateOrTenant::getAuthUserFindingPost();
+
+        return $findPost;
+    }
+
+    public function index() {
+        // Call the getUserAuthInfo method in the User model to fetch the info of the authenticated user
+        $userInfo = User::getUserAuthInfo();
+
+        // Call the propertyPost method to fetch the property post of an authenticated user
+        $properties = $this->propertyPost();
+
+        // Call the findRoommateOrTenantPost() method to fetch the post for the authenticated user
+        $findingPost = $this->findRoommateOrTenantPost();
+
         return view('user-profile', [
-            'user' => Auth::user(),
-            'properties' => $propertyPost
+            'user' => $userInfo
+            ,'propertyPost' => $properties
+            ,'findPost' => $findingPost
         ]);
     }
 }
