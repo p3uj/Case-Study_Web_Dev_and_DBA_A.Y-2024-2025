f1e7b3830d9cd288fa64608f38730e706196cf2c
Bengie Villesco <bvillesco@gmail.com>
2025-01-18 22:04:48 +0800

    Added a store method to insert the submitted form data into the database.

--- app/Http/Controllers/FindRommateOrTenantController.php
+++ app/Http/Controllers/FindRommateOrTenantController.php
@@ -8,6 +8,7 @@ use App\Models\FindRoommateOrTenant;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use Carbon\Carbon;
 
 class FindRommateOrTenantController extends Controller
 {
@@ -27,5 +28,22 @@ class FindRommateOrTenantController extends Controller
             ,'barangays' => $barangay
             ,'posts' => $usersPosts
         ]);
+    }
+
+    // Insert data into the 'find_roommate_or_tenants' table in the database.
+    public function store(Request $request) {
+        $searchingPost = new FindRoommateOrTenant();
+        $searchingPost->user_id = Auth::id();
+        $searchingPost->date_posted = Carbon::now();
+        $searchingPost->city = $request->city;
+        $searchingPost->barangay = $request->barangay;
+        $searchingPost->description = $request->description;
+        $searchingPost->category_finding = (Auth::user()->role == 'Tenant' ? 'Roommate' : 'Tenant');
+
+        if ($searchingPost->save()) {
+            return redirect()->back();
+        }
+
+        return "Error occured";
     }
 }
