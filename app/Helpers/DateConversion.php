f7ea9ee29686349b64496c454cd2551f082b056e
Bengie Villesco <bvillesco@gmail.com>
2025-01-19 16:30:20 +0800

    Create DateConversion class to convert dates to Carbon instances and provide a readable format for reuse across files

---
+++ app/Helpers/DateConversion.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Support\Carbon;
+
+class DateConversion
+{
+    public static function formatDate($records, $dateColumn)
+    {
+        // Convert date to Carbon for formatting the date into readable format
+        foreach ($records as $record) {
+            $record->$dateColumn = Carbon::parse($record->$dateColumn); // Converting the date to a Carbon instance for easier date manipulation
+            $record->$dateColumn = $record->date_posted->format('F d, Y \a\t h:i A'); // Formatting the date to Month, Day, Year, 'at', Time (AM/PM)
+        }
+
+        return $records;
+    }
+}
