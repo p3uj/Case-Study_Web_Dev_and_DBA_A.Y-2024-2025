42ed1928a2102790c6d676783a79cfb6516d9bb8
Bengie Villesco <bvillesco@gmail.com>
2025-01-21 01:54:59 +0800

    Added statement to create stored procedure for retrieving all the reviews based on the user id

--- database/migrations/2024_12_30_084943_create_reviews.php
+++ database/migrations/2024_12_30_084943_create_reviews.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -19,6 +20,26 @@ return new class extends Migration
             $table->text('review_text')->nullable();
             $table->timestamp('created_at')->useCurrent();
         });
+
+        // Create stored procedure
+        // Define stored procedure to get all reviews based on the user id
+        DB::statement("
+            CREATE PROCEDURE GetAllReviewsByUserId
+                @userId BIGINT
+            AS
+            BEGIN
+                SELECT reviews.*, ReviewByUser.firstname, ReviewByUser.lastname
+                FROM reviews
+                INNER JOIN property_posts AS PPost
+                    ON PPost.id = reviews.property_post_id
+                INNER JOIN users AS PPostOwner
+                    ON PPostOwner.id = PPost.user_id
+                INNER JOIN users AS ReviewByUser
+                    ON ReviewByUser.id = reviews.user_id
+                WHERE PPostOwner.id = @userId
+                ORDER BY created_at DESC
+            END
+        ");
     }
 
     /**
@@ -26,7 +47,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        // Drop the stored procedure
+        DB::statement("DROP PROCEDURE IF EXISTS GetAllReviewsByUserId");
+
         Schema::dropIfExists('reviews');
     }
 };
