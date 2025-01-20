ff7d0e3817816322cb7d04d2d25aba8ba8dc3458
Bengie Villesco <bvillesco@gmail.com>
2025-01-21 01:54:10 +0800

    Use dateColumn parameter for accessing the field in the record for date format

--- app/Helpers/DateConversion.php
+++ app/Helpers/DateConversion.php
@@ -11,7 +11,7 @@ class DateConversion
         // Convert date to Carbon for formatting the date into readable format
         foreach ($records as $record) {
             $record->$dateColumn = Carbon::parse($record->$dateColumn); // Converting the date to a Carbon instance for easier date manipulation
-            $record->$dateColumn = $record->date_posted->format('F d, Y \a\t h:i A'); // Formatting the date to Month, Day, Year, 'at', Time (AM/PM)
+            $record->$dateColumn = $record->$dateColumn->format('F d, Y \a\t h:i A'); // Formatting the date to Month, Day, Year, 'at', Time (AM/PM)
         }
 
         return $records;
