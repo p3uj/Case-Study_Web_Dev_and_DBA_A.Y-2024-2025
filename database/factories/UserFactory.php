57c7461ed2619a9681a99347d1cc7174c64e9afd
Edzra Macas <EdzraM7@gmail.com>
2025-01-22 23:40:21 +0800

    added user seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -14,7 +15,7 @@ class UserFactory extends Factory
     /**
      * The current password being used by the factory.
      */
-    protected static ?string $password;
+    protected $model = User::class;
 
     /**
      * Define the model's default state.
@@ -24,21 +25,40 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'role' => $this->faker->randomElement(['Landlord', 'Tenant']),
+            'firstname' => $this->faker->firstName(),
+            'lastname' => $this->faker->lastName(),
+            'city' => $this->faker->city(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'password' => Hash::make('1234'),
+            'profile_photo_path' => 'resources/images/sampleProfile.png',
+            'bio' => $this->faker->text(100),
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
+/**
+     * Indicate that the user is a landlord.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
      */
-    public function unverified(): static
+    public function landlord(): static
     {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
+        return $this->state([
+            'role' => 'Landlord',
+            'bio' => 'Landlord managing multiple properties.',
+        ]);
+    }
+
+    /**
+     * Indicate that the user is a tenant.
+     *
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function tenant(): static
+    {
+        return $this->state([
+            'role' => 'Tenant',
+            'bio' => 'Tenant looking for affordable housing.',
         ]);
     }
 }
