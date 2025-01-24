5a2df35f689401c8f03a643197eda0a201a75872
Bengie Villesco <bvillesco@gmail.com>
2025-01-25 03:08:06 +0800

    Added view to get all the active property posts with usernames

---
+++ database/migrations/2025_01_25_030353_add_view_to_get_all_active_property_posts_with_usernames.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //-- CREATE VIEW TO GET ALL PROPERTY POST WITH USERNAME WHERE POST IS NOT DELETED
+        DB::statement("
+            CREATE VIEW RE_V_GET_ALL_ACTIVE_PROPERTY_POST_WITH_USERNAME
+            AS
+            SELECT
+                PPost.*
+                ,PostedByUser.firstname + ' ' + PostedByUser.lastname AS UserName
+            FROM property_posts AS PPost
+            INNER JOIN users AS PostedByUser
+                ON PostedByUser.id = PPost.user_id
+            WHERE
+                PPost.is_deleted = 0
+        ");
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        // Drop the view
+        DB::statement("DROP VIEW IF EXISTS RE_V_GET_ALL_ACTIVE_PROPERTY_POST_WITH_USERNAME");
+    }
+};
