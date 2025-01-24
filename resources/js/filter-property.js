2d75aab61957998d68823d77058181f958e074ff
Bengie Villesco <bvillesco@gmail.com>
2025-01-25 02:44:47 +0800

    Refactor: Remove unnecessary codes and add codes to set the option filter based on their data attribute.

--- resources/js/filter-property.js
+++ resources/js/filter-property.js
@@ -1,32 +1,21 @@
 // Filter search
 // Get the value of the selected filter unit category
 document.addEventListener("DOMContentLoaded", function () {
-    var unitCategoryDropdown = document.getElementById("filter-unit-category"); // Get the element that has the id of 'filter-unit-category'
+    var unitCategoryDropdown = document.getElementById('filter-unit-category'); // Get the element that has the id of 'filter-unit-category'
+    var cityDropdown = document.querySelector('.filter-city');
     var filterRentalPriceInput = document.getElementById('filter-rental-price'); // Get the element that has the id of 'filter-rental-price'
 
-    unitCategoryDropdown.addEventListener("change", function () {
-        var selectedOption = this.options[this.selectedIndex]; // Get the selected option
-        var filterUnitCategoryValue = selectedOption.value; // Get the value of the selected option
+    // Set the option of filter unit category dropdown based on the 'data-filter-unit-category' value
+    unitCategoryDropdown.value = unitCategoryDropdown.getAttribute('data-filter-unit-category');
 
-        console.log("Selected filter unit category:", filterUnitCategoryValue);
-    });
+    // Reset the dropdown to the placeholder option if its value is null or empty
+    if (!cityDropdown.value) {
+        cityDropdown.selectedIndex = 0;  // Set to the first option which is the 'Select city'
+    }
 
-    // Get the value of the selected filter city
-    document.querySelectorAll(".filter-city").forEach(function (dropdown) {
-        dropdown.addEventListener("change", function () {
-            var selectedOption = this.options[this.selectedIndex]; // Get the selected option
-            var filterCityValue = selectedOption.value; // Get the value of the selected option
+    // Set the option of filter city dropdown based on the 'data-filter-unit-category' value
+    cityDropdown.value = cityDropdown.getAttribute('data-filter-city');
 
-            console.log("Selected filter city:", filterCityValue);
-
-            this.setAttribute("data-filter-city-value", filterCityValue);
-        });
-    });
-
-    // Get the value of the filter rental price
-    filterRentalPriceInput.addEventListener("input", function () {
-        var filterRentalPriceValue = filterRentalPriceInput.value; // Get the value of the filter rental price
-
-        console.log('filter rental price value:', filterRentalPriceValue);
-    });
+    // Set the option of filter rental price based on the 'data-filter-rental-price' value
+    filterRentalPriceInput.value = filterRentalPriceInput.getAttribute('data-filter-rental-price');
 });
