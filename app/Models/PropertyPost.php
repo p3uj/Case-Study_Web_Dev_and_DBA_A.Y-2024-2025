036caa8eea24ddd3b3f26cb72a6244507b2de82b
Bengie Villesco <bvillesco@gmail.com>
2025-01-25 16:46:46 +0800

    Rename the method from getAllPropertyPostByFilterSearch to getAllFilteredPropertyPosts.

--- app/Models/PropertyPost.php
+++ app/Models/PropertyPost.php
@@ -42,7 +42,7 @@ class PropertyPost extends Model
         return $authUserPropertyPosts;
     }
 
-    public static function getAllPropertyPostByFilterSearch($unitCategory = null, $city = null, $rentalPrice = null) {
+    public static function getAllFilteredPropertyPosts($unitCategory = null, $city = null, $rentalPrice = null) {
         $propertyPosts = DB::select('RE_SP_GET_ALL_PROPERTY_POST_BASED_ON_FILTER_SEARCH ?, ?, ?', [
             $unitCategory
             ,$city
