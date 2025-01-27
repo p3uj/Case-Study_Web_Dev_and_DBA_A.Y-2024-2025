44324eefa160d541052f9637d7b8198c4faadca6
Bengie Villesco <bvillesco@gmail.com>
2025-01-28 02:07:41 +0800

    Delete edit-form.js

--- resources/js/edit-form.js
+++
@@ -1,9 +0,0 @@
-function updatePopoverDataId(button) {
-    // Get the data-id from the button
-    var dataId = button.getAttribute('data-id');
-    console.log('button data id:', dataId);
-
-    // Get the popover div and update its data-id attribute
-    var popover = document.querySelector('[popover]');
-    popover.setAttribute('data-id', dataId);
-}
