a82e927049412305b021c4e7015455d0aa53a903
JohannaSillano <sillanojohannamae@gmail.com>
2025-01-30 01:02:40 +0800

    Edit-property-post and edit-find-roomate-tenant (UI)

--- resources/views/EditForms/edit-property-post.blade.php
+++ resources/views/EditForms/edit-property-post.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Edit Property Post</title>
+    <link rel="stylesheet" href="{{ Vite::asset('resources/css/edit-property-post.css') }}">
 
     @vite('resources/js/edit-dropdown.js')
 </head>
@@ -15,6 +16,9 @@
     <form id="form" action="{{ route('editpropertypost.post') }}" method="post">
         @csrf
         <div class="form-container">
+            <!-- Close button with the correct route -->
+            <a href="{{ route('user-profile') }}" class="close-btn" onclick="return confirm('Are you sure you want to leave? Any unsaved changes will be lost.')">&times;</a>
+
             <!-- Hidden input to hold the id for the server side -->
             <input type="hidden" name="property-post-id" value="{{ $propertyPostId }}">
             <input type="hidden" name="property-info-id" value="{{ $propertyInfoId }}">
@@ -36,11 +40,11 @@
             <!-- Unit Category Dropdown Box -->
             <select name="unit-category" id="unit-category" required>
                 <option value="" disabled selected>Please select a unit category</option>
-                <option value="Dormitories" {{ $post->unit_category == "Dormitories"}} ? @selected(true) : @selected(false)>Dormitories</option>
-                <option value="Studio Units" {{ $post->unit_category == "Studio Units"}} ? @selected(true) : @selected(false)>Studio Units</option>
-                <option value="Apartments" {{ $post->unit_category == "Apartments"}} ? @selected(true) : @selected(false)>Apartments</option>
-                <option value="Boarding Houses" {{ $post->unit_category == "Boarding Houses"}} ? @selected(true) : @selected(false)>Boarding Houses</option>
-                <option value="Other" {{ $post->unit_category == "Other"}} ? @selected(true) : @selected(false)>Other</option>
+                <option value="Dormitories" {{ $post->unit_category == "Dormitories" ? 'selected' : '' }}>Dormitories</option>
+                <option value="Studio Units" {{ $post->unit_category == "Studio Units" ? 'selected' : '' }}>Studio Units</option>
+                <option value="Apartments" {{ $post->unit_category == "Apartments" ? 'selected' : '' }}>Apartments</option>
+                <option value="Boarding Houses" {{ $post->unit_category == "Boarding Houses" ? 'selected' : '' }}>Boarding Houses</option>
+                <option value="Other" {{ $post->unit_category == "Other" ? 'selected' : '' }}>Other</option>
             </select>
 
             <!-- City Dropdown Box -->
@@ -60,20 +64,17 @@
             <!-- Barangay Dropdown Box -->
             <select name="barangay" id="barangay" data-barangay-list="{{ json_encode($barangays) }}"
                 data-fetch-barangay="{{ $post->barangay }}">
-                <!-- This dropdown should be dynamic based on the selected option in the city.
-                            You can use this for API call to get the barangays based on the id(stored as cityCode) of selected option in the city.
-                            https://psgc.gitlab.io/api/cities/{cityCode}/barangays/
-                        -->
                 <option value="" disabled selected>Please select barangay</option>
-                <!-- Barangay options will be populated by property.js  -->
             </select>
-
+            
             <!-- Input Fields -->
-            <input type="number" placeholder="Rental Price" id="rental-price" name="rental-price" value="{{ $post->rental_price }}">
-            <input type="number" placeholder="Maximum Occupancy" id="max-occupancy" name="max-occupancy" value="{{ $post->max_occupancy }}">
+            <input type="number" class="input-field" placeholder="Rental Price" id="rental-price" name="rental-price" value="{{ old('rental-price', $post->rental_price) }}">
+
+            <input type="number" class="input-field" placeholder="Maximum Occupancy" id="max-occupancy" name="max-occupancy" value="{{ old('max-occupancy', $post->max_occupancy) }}">
+
             <textarea class="description" placeholder="Write a description" id="description" name="description">{{ $post->description }}</textarea>
 
-            <button class="post-btn">Post</button>
+            <button type="submit" class="save-btn" onclick="return confirm('Are you sure you want to save these changes?')">Save Changes</button>
         </div>
     </form>
 </body>
