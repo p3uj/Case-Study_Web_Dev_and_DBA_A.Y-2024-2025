b9d47fbf551a15614448fb1983d4b9c742128f6b
Bengie Villesco <bvillesco@gmail.com>
2025-01-30 02:53:45 +0800

    Update

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -24,7 +24,8 @@ return new class extends Migration
             $table->string('profile_photo_path');
             $table->text('bio');
             //$table->rememberToken();
-            $table->timestamps();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
