2d8f7aac2d7fb336966f45dd3fbed21aee5746dd
BengieVillesco <bvillesco@gmail.com>
2026-05-19 02:39:24 +0800

    feat: add logo with background and favicon icon

--- resources/views/Auth/register.blade.php
+++ resources/views/Auth/register.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="{{ Vite::asset('resources/images/RentEaseLogoWithBG.png') }}" type="image/png">
     <title>RentEase Register</title>
     
     <!-- Link css file -->
--- resources/views/EditForms/edit-profile.blade.php
+++ resources/views/EditForms/edit-profile.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" href="{{ Vite::asset('resources/images/RentEaseLogoWithBG.png') }}" type="image/png">
     <title>Edit Searching Post</title>
 
     @vite('resources/css/edit-profile.css')
